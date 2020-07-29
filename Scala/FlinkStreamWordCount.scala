b0cd1694bf3a8d25aa897fdf1b7dcce51739eb3b
ZB <35168357+CruiseZhang@users.noreply.github.com>
2020-07-29 22:21:53 +0800

    Update FlinkStreamWordCount.scala

--- Scala/FlinkStreamWordCount.scala
+++ Scala/FlinkStreamWordCount.scala
@@ -1,31 +1,28 @@
 package com.bjsxt.flink
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import java.net.URL
 
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
+import org.apache.flink.api.scala._ // 导入隐式转换
 /**
-  * 基于Flink流计算的WordCount案例
+  * Flink批计算案例，相比spark批处理没有优势，故不详细讲解
   */
-object FlinkStreamWordCount {
+object BatchWordCount {
 
   def main(args: Array[String]): Unit = {
-    //1、初始化Flink流计算的环境
-    val streamEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    //修改并行度
-    streamEnv.setParallelism(1) //默认所有算子的并行度为1
-    //2、导入隐式转换
-    import org.apache.flink.streaming.api.scala._ //_是导入scala包下的所有对象
-    //3、读取数据,读取sock流中的数据
-    val stream: DataStream[String] = streamEnv.socketTextStream("hadoop101",8888) //DataStream和spark中Dstream类似
+    //初始化Flink批处理环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
 
-    //4、转换和处理数据
-    val result: DataStream[(String, Int)] = stream.flatMap(_.split(" "))
-      .map((_, 1)).setParallelism(2)
-      .keyBy(0)//分组算子: 0 或者 1 代表下标。前面的DataStream[二元组] , 0代表单词 ，1代表单词出现的次数
-      .sum(1).setParallelism(2) //聚合累加算子。此处对1进行累加，即对单词出现的次数进行累加
+    val dataPath: URL = getClass.getResource("/wc.txt") //使用相对路径来得到完整的文件路径
 
-    //5、打印结果
-    result.print("结果").setParallelism(1)
-    //6、启动流计算程序
-    streamEnv.execute("wordcount")
+    //读数据
+    val data: DataSet[String] = env.readTextFile(dataPath.getPath) //DataSet 和 spark RDD类似
+
+    //计算并且打印结果
+    data.flatMap(_.split(" "))
+      .map((_,1))
+      .groupBy(0)
+      .sum(1)
+      .print()
   }
 }
