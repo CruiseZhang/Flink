4a62ce377b4b0597ce41eced354be6c36affdb5b
ZB <35168357+CruiseZhang@users.noreply.github.com>
2020-07-29 21:11:37 +0800

    Update FlinkStreamWordCount.scala

--- Scala/FlinkStreamWordCount.scala
+++ Scala/FlinkStreamWordCount.scala
@@ -13,7 +13,7 @@ object FlinkStreamWordCount {
     //修改并行度
     streamEnv.setParallelism(1) //默认所有算子的并行度为1
     //2、导入隐式转换
-    import org.apache.flink.streaming.api.scala._
+    import org.apache.flink.streaming.api.scala._  //_是导入scala包下的所有对象
     //3、读取数据,读取sock流中的数据
     val stream: DataStream[String] = streamEnv.socketTextStream("hadoop101",8888) //DataStream和spark中Dstream类似
 
